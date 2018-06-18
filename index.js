52dff4fb48c7295bdf8316c552292c7dd51c33cd
Hania Bigo <hrbigos@umich.edu>
2018-06-18 15:23:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
- return kittens.slice(0, kittens.length -3);
+ return kittens.slice(0, kittens.length -1);
 }
 
 function removeFirstKitten() {
